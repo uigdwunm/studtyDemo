54003b8a74bd9cc73d4e2374bf9e6813965f0e9a
zhaolaiyuan <704775924@qq.com>
2020-07-19 12:07:47 +0800

    提交

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,16 +1,48 @@
 import explain.HashMap;
+import explain.Map;
+import leetCode.utils.GsonUtils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Solution {
-    public int[] avoidFlood(int[] rains) {
-        return rains;
+    public int[] countSubTrees(int n, int[][] edges, String labels) {
+        Map<Integer, Integer> map = new HashMap<>(n);
+        boolean[] exist = new boolean[n];
+        for (int[] edge : edges) {
+            exist[edge[0]] = true;
+            map.put(edge[1], edge[0]);
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (!exist[i]) {
+                // 是起点
+                this.to(labels.charAt(i), i, result, map, labels);
+            }
+        }
+
+        return result;
+    }
+
+    private void to(char c, int i, int[] result, Map<Integer, Integer> map, String labels) {
+        result[i] += 1;
+        Integer t = map.get(i);
+        while (t != null) {
+            if (result[t] == 0) {
+                this.to(labels.charAt(t), t, result, map, labels);
+            }
+            if (labels.charAt(t) == c) {
+                result[t] += 1;
+            }
+
+            t = map.get(t);
+        }
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
+        int[][] ints = GsonUtils.convertToIntArrArr("[[0,2],[0,3],[1,2]]");
+        int[] abaedcds = solution.countSubTrees(4, ints, "aeed");
+        System.out.println(Arrays.toString(abaedcds));
     }
 }
